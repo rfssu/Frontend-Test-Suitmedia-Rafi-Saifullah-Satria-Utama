d36cad46e16d9c6e51c11be7549601291c6fc926
rfssu <rafi.saifullah.ft20@mail.umy.ac.id>
2023-12-24 03:50:35 +0700

    initiate

---
+++ src/components/Navbar.vue
@@ -0,0 +1,70 @@
+<template>
+
+  <nav class="navbar fixed-top navbar-expand-lg " style="background-color: #ff6800;">
+    <div class="container">
+      <a class="navbar-brand" href="#">
+        <img src="../assets/suitmedialogo.png" alt="Logo" width="70" height="35">
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="nav ml-auto">
+          <li class="nav-item">
+              <a class="nav-link" href="#">Work</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Services</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Ideas</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Careers</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contact</a>
+            </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+
+</template>
+
+<script>
+export default {
+    name: 'NavBar',
+}
+/* <ul class="nav ml-auto nav-underline">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Work</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Ideas</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Careers</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact</a>
+          </li>
+        </ul>
+*/
+    
+</script>
+
+<style>
+
+
+</style>
---
+++ src/views/IdeasView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="Ideas">
+    <Navbar />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Navbar from '@/components/Navbar.vue'
+
+export default {
+  name: 'IdeasView',
+  components: {
+    Navbar
+  }
+}
+</script>
